e0d192aff07aaa89ccbab142a2e9ff88a3ed48b9
Mark Ericksen <brainlid@gmail.com>
2021-10-05 14:34:52 -0600

    updated to use published fly_rpc

--- fly_postgres/mix.exs
+++ fly_postgres/mix.exs
@@ -26,7 +26,7 @@ defmodule FlyPostgres.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:fly_rpc, git: "https://github.com/superfly/fly_rpc_elixir.git", branch: "main"},
+      {:fly_rpc, "~> 0.1"},
       {:postgrex, ">= 0.0.0"},
       {:ex_doc, "~> 0.25", only: :dev}
     ]
