5b075fd04eb1913afc117a61ea47d852c7bea5fe
Mark Ericksen <brainlid@gmail.com>
2021-10-09 22:08:55 -0600

    update dep version

--- fly_postgres/mix.exs
+++ fly_postgres/mix.exs
@@ -4,7 +4,7 @@ defmodule FlyPostgres.MixProject do
   def project do
     [
       app: :fly_postgres,
-      version: "0.1.5",
+      version: "0.1.6",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       name: "Fly Postgres",
@@ -26,7 +26,7 @@ defmodule FlyPostgres.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:fly_rpc, "~> 0.1.3"},
+      {:fly_rpc, "~> 0.1.4"},
       {:postgrex, ">= 0.0.0"},
       {:ex_doc, "~> 0.25", only: :dev}
     ]
