0b446fcc9afc476d573ceb0875deb4b0550cceb4
Mark Ericksen <brainlid@gmail.com>
2021-10-25 15:24:27 -0600

    Only fly_rpc in the git repo (#3)
    * standardized the verbose logs better
    
    * change default LSN timeout delay to 5 seconds
    - matches that of the RPC timeout
    
    * allow not waiting for replication
    - created Fly.rpc_primary/4 as convenience function
    - fly_postgre README updated for example usage
    
    * readme updates
    
    * single hex package in the repo
    - remove fly_postgres package from the repo
    - move files from fly_rpc directory up to root

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fly.MixProject do
   def project do
     [
       app: :fly_rpc,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       name: "Fly RPC",
