e2c6594889b39d18e3c59d95db5d81e5c2fd7b68
Mark Ericksen <brainlid@gmail.com>
2023-08-12 00:55:22 -0600

    release-v-0-2-1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fly.MixProject do
   def project do
     [
       app: :fly_rpc,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       name: "Fly RPC",
